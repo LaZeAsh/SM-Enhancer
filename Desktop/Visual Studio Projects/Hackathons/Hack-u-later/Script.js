4d7e694bf01c020259e93b2c5e97159f08a6da3a
Brayton-Lordianto <braylord30@gmail.com>
2022-12-31 16:31:02 -0800

    extension works with a button that fills the comment section

--- Desktop/Visual Studio Projects/Hackathons/Hack-u-later/Script.js
+++ Desktop/Visual Studio Projects/Hackathons/Hack-u-later/Script.js
@@ -1,20 +1,38 @@
-console.log("hi")
-function print(txt) {
-    console.log(txt)
+function placeInComment(text) {
+    var list2 = document.querySelectorAll('div[class="editor-content ql-container"] > div')
+    console.log(list2.length)
+    for (var i = 0; i < list2.length; ++i) {
+        // get the closest id of the comment 
+        console.log(list2[i].closest("div[class]"))
+        var p = `<p>${text}</p>`;
+        list2[i].innerHTML = p;    
+    }
 }
+
+
+function print(e) {
+    console.clear()
+    var text = document.elementFromPoint(e.clientX, e.clientY).getAttribute("text")
+    console.log(text)
+    placeInComment(text)
+}
+
 function getText() {
-    // var texts = document.querySelectorAll('span[class="break-words"] > span > span')
     var parents = document.querySelectorAll('div[class="update-components-text relative feed-shared-update-v2__commentary "]')
-    var texts = document.querySelectorAll('div[class="update-components-text relative feed-shared-update-v2__commentary "] > span > span > span')
-    console.log(texts.length)
-    for (var i = 0; i < texts.length; ++i) {
+    for (var i = 0; i < parents.length; ++i) {
+        // to find text it's a linkedin thing 
         var parent = parents[i]
-        var text = texts[i].innerText
-        var button = `<button onclick={print(${text})}">hi</button>`
-        console.log(parent.childNodes.length)
+        var text = parent.children[0].children[0].innerText
+        if (text === undefined || text === null) continue; 
+
+        // we use different button ids to simulate different pressed
+        var buttonId = `SendToServerBtn${i}`
+        var button = `<button id=${buttonId} text="${text}">hi</button>`
+
         // default linkedin parent of texts have 5 children
         if (parent.childNodes.length == 5) {
             parent.innerHTML += button
+            document.getElementById(`${buttonId}`).addEventListener("click", print);
         }
     }
 }
---
+++ Desktop/Visual Studio Projects/Hackathons/Hack-u-later/buttonFunction.js
@@ -0,0 +1,8 @@
+document.getElementById("SendToServerBtn1").addEventListener("click", func);
+document.getElementById("SendToServerBtn2").addEventListener("click", func);
+document.getElementById("SendToServerBtn3").addEventListener("click", func);
+document.getElementById("SendToServerBtn4").addEventListener("click", func);
+
+function func() {
+    console.log("hi")
+}
